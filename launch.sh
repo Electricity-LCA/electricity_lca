f565122366cdc5ad7e7c1a5b58be3f5cfb128607
artur <artur.do@proton.me>
2024-02-21 15:14:32 +0100

    Update launch script to activate environment

--- launch.sh
+++ launch.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+source venv/bin/activate
+
 streamlit run src/visualization/visualize.py &
-uvicorn src.microservice.main:app --reload
+uvicorn src.microservice.main:app --reload --host 0.0.0.0 --port 8000
