ee41dc5c28540a8de6ddac2f6adb7cd0ffe0a31e
artur <artur.do@proton.me>
2024-02-28 10:07:31 +0100

    Update microservice with calculation engine and list endpoints

--- src/microservice/main.py
+++ src/microservice/main.py
@@ -1,19 +1,19 @@
 import logging
 import os
+from typing import Any
 
 import pandas as pd
 import sqlalchemy as sqla
 from dotenv import load_dotenv
-from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI, Response
 from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 
-
 from src.data.get_common_data import load_common_data_from_db
-from src.orm.base import ElectricityGeneration, Regions
+from src.microservice.calculate import ImpactResultSchema, calculate_impact_df
+from src.microservice.constants import ServerError
+from src.microservice.generation import get_electricity_generation_df
 
-ROW_LIMIT = 500
 load_dotenv()
 HOST = os.getenv('ELEC_LCA_HOST')
 DB_NAME = os.getenv('ELEC_LCA_DB_NAME')
@@ -33,50 +33,67 @@ cache = load_common_data_from_db(sql_engine=engine)
 app = FastAPI()
 
 
+def cache_data(f):
+    cache = {}
+
+    def foo(x):
+        if x not in cache:
+            cache[x] = f(x)
+        return cache[x]
+
+    return foo
+
+
 @app.get('/docs')
 def get_docs():
     return get_swagger_ui_html(title='API documentation for elc_lca')
 
+
+@app.get('/list_regions')
+async def list_regions():
+    regions_df = cache.regions
+    return Response(regions_df.to_json(orient='records'), media_type="application/json")
+
+@app.get('/list_generation_types')
+async def list_generation_types():
+    generation_types_df = cache.generation_types
+    return Response(generation_types_df.to_json(orient='records'), media_type="application/json")
+
+@app.get('/list_generation_type_mappings')
+async def list_generation_type_mappings():
+    generation_type_mappings_df = cache.generation_type_mappings
+    return Response(generation_type_mappings_df.to_json(orient='records'), media_type="application/json")
+
 @app.get('/generation')
 async def get_electricity_generation(date_start, region_code: str, generation_type_id: int):
-    load_dotenv()
+    try:
+        df = await get_electricity_generation_df(date_start, region_code, generation_type_id,engine=engine)
+    except TypeError as e:
+        return Response(status_code=400, content=str(e))
+    except ValueError as e:
+        return Response(status_code=422, content=str(e))
+    except ServerError as e:
+        return Response(status_code=500, content=str(e))
+    if not isinstance(df, pd.DataFrame):
+        return Response(status_code=500)
+    return Response(df.to_json(), media_type="application/json")
 
-    HOST = os.getenv('ELEC_LCA_HOST')
-    DB_NAME = os.getenv('ELEC_LCA_DB_NAME')
-    USER = os.getenv('ELEC_LCA_USER')
-    PASSWORD = os.getenv('ELEC_LCA_PASSWORD')
 
-    if not isinstance(region_code, str):
-        return None
-
-    engine = sqla.create_engine(sqla.engine.url.URL.create(
-        drivername='postgresql',
-        host=HOST,
-        database=DB_NAME,
-        username=USER,
-        password=PASSWORD
-    ))
-    session_obj = sessionmaker(bind=engine)
-    with session_obj() as session:
-        region_id_query = session.query(Regions.Id).where(Regions.Code == region_code).limit(1)
-        region_id_df = pd.read_sql(region_id_query.statement, session.bind)
-        if region_id_df is None or region_id_df.shape[0] != 1:
-            raise ValueError(f'Region Code `{region_code}` could not be found in database')
-
-        region_id = int(region_id_df.iat[0, 0])
-        logging.warning(f'REGION IS {region_id}')
-
-        query = (session.query(ElectricityGeneration)
-                 .where(ElectricityGeneration.GenerationTypeId == generation_type_id)
-                 .where(ElectricityGeneration.RegionId == region_id)
-                 .limit(ROW_LIMIT))
-
-        df = pd.read_sql(query.statement, session.bind)
-
-        return Response(df.to_json(), media_type="application/json")
+@app.get('/calculate', response_model=ImpactResultSchema)
+async def calculate_impact(date_start, region_code: str, generation_type_id: int)->Any:
+    try:
+        impact_df = await calculate_impact_df(date_start, region_code, generation_type_id,engine=engine)
+    except TypeError as e:
+        return Response(status_code=400, content=str(e))
+    except ValueError as e:
+        return Response(status_code=422, content=str(e))
+    except ServerError as e:
+        return Response(status_code=500, content=str(e))
+    if not isinstance(impact_df, pd.DataFrame):
+        return Response(status_code=500)
+    return Response(impact_df.to_json(), media_type='text/json')
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,filename='api.log')
     uvicorn.run(app, port=8000)
-
-
